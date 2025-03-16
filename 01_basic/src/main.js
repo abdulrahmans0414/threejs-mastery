16fe911c20c441bdf52c8877ce7caad8530a73e2
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-16 20:06:17 +0530

    add basic Three.js scene with a red cube

--- 01_basic/src/main.js
+++ 01_basic/src/main.js
@@ -1,3 +1,22 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+// console.log(THREE);
+
+const scene = new THREE.Scene();
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial,
+)
+
+// console.log(cubeMesh);
+// console.log(scene);
+
+scene.add(cubeMesh);
+console.log(scene);
+
+
+
